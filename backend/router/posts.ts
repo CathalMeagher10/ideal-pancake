a301566f1ba292b1c15fde153714096d2cefcd84
BuildTools <c.meagher3@nuigalway.ie>
2022-06-02 14:01:06 +0100

    updated schemas

--- backend/router/posts.ts
+++ backend/router/posts.ts
@@ -3,8 +3,19 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
 import { prisma } from "../../db/client";
 
-export const postsRouter = trpc.router().query("get-all", {
-  async resolve() {
-    return await prisma?.post.findMany();
-  },
-});
+export const postsRouter = trpc
+  .router()
+  .query("get-all", {
+    async resolve() {
+      return await prisma.post.findMany();
+    },
+  })
+  .mutation("create-post", {
+    input: z.object({ title: z.string(), author: z.string() }),
+    async resolve({ input }) {
+      // create post
+      return await prisma.post.create({
+        data: { name: input.title, author: input.author, body: "post body" },
+      });
+    },
+  });
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,54 @@ import { useEffect } from "react";
 import { prisma } from "../db/client";
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
-
+import { useState } from "react";
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["posts.get-all"]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const createPost = trpc.useMutation(["posts.create-post"]);
 
   if (isLoading || !data) {
     return <div>loading...</div>;
   }
-  return <div>{data[0]?.author}</div>;
+
+  const renderPosts = () => {
+    return data.map((post) => {
+      return (
+        <div key={post.id}>
+          <p> author: {post.author}</p>
+          <p>createdAt: {post.createdAt.toString()},</p>
+          <p> id: {post.id} </p>
+        </div>
+      );
+    });
+  };
+
+  const handleCreatePost = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    createPost.mutate({ author: author, title: title });
+  };
+  return (
+    <div>
+      {renderPosts()}
+      <form onSubmit={handleCreatePost}>
+        <input
+          type="text"
+          placeholder="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button>create new post</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
